98ffbdaf6b11bdc60cb8bbaa3d195dc56bf1cc69
Vel <avior@gmail.com>
2023-12-13 18:36:58 +0200

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	README.md

--- src/CW7_2.java
+++ src/CW7_2.java
@@ -3,25 +3,10 @@ import java.util.ArrayList;
 public class CW7_2 {
 
     public static void main(String[] args) {
-        Engnine engnine = new Engnine();
-        GreenEnergy greenEnergy = new GreenEnergy();
-        Robot masha = new Robot("Masha",greenEnergy);
-
-        masha.cooking();
-
-        Robot den = new Robot("Den",engnine);
-        den.cooking();
-
-        Robot kos = new RobotBoutyMaker("Kostya",engnine);
-        kos.cooking();
-//        kos.addCocos();
-
-        ArrayList<Robot> list = new ArrayList<>();
-        list.add(kos);
-        list.add(den);
-
-
+        System.out.println("hello world");
     }
+
+
 
 }
 
